c0214c9a89b1e79544f8c4dff812b7afd2b464da
IW.MOON <=>
2021-06-25 22:46:50 +0900

    Initial commit

---
+++ src/main/java/com/trv/trvapp/TrvappApplication.java
@@ -0,0 +1,13 @@
+package com.trv.trvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrvappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrvappApplication.class, args);
+	}
+
+}
